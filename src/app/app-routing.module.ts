dc42133db061ae64348fd66571e8db1ceb7a129c
LebrunLaurentDylan <131178706+LebrunLaurentDylan@users.noreply.github.com>
2024-07-03 23:03:48 +0200

    ajout de la page a propos pour test de style. routing de page accueil et page à propos, ajout dans le header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AproposComponent } from './apropos/apropos.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'apropos', component: AproposComponent },
+  { path: 'accueil', component: AccueilComponent },
+  { path: '', redirectTo: '/accueil', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
