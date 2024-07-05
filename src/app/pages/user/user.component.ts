363eb20c215a77950891d3dbe77ba7c43b6c9334
LebrunLaurentDylan <131178706+LebrunLaurentDylan@users.noreply.github.com>
2024-07-05 15:27:56 +0200

    change on user

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -10,11 +10,6 @@ import { ServiceCrudUserService } from 'src/app/services/service-crud-user.servi
 })
 export class UserComponent {
   user = new User('', '', '', '');
-  id: number = 0;
-  nom: string = '';
-  prenom: string = '';
-  email: string = '';
-  telephone: string = '';
   constructor(private srv: ServiceCrudUserService) {}
   ngOnInit(): void {
     let str = sessionStorage.getItem('user');
@@ -25,8 +20,21 @@ export class UserComponent {
     let email = this.user.email;
     let telephone = this.user.telephone;
   }
-
+  nom = '';
+  prenom = '';
+  email = '';
+  telephone = '';
+  id = 0;
+  mettreAJour() {
+    this.user.nom = this.nom;
+    this.user.prenom = this.prenom;
+    this.user.email = this.email;
+    this.user.telephone = this.telephone;
+    this.srv.UpdateUser(this.user);
+  }
   deleteUser() {
     this.srv.DeleteUser(this.id);
+    console.log(this.id);
+    
   }
 }
