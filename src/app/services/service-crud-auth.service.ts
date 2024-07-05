db48973ee1fb17f00795c56f9deaf3514207c723
LebrunLaurentDylan <131178706+LebrunLaurentDylan@users.noreply.github.com>
2024-07-05 15:33:32 +0200

    creation du service auth

---
+++ src/app/services/service-crud-auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ServiceCrudAuthService {
+  constructor(private http: HttpClient) {}
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+}
