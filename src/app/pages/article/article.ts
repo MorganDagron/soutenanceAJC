4b9a11ec07dfdf82bcc4c40c8b46b9552e285a39
morgan <morgan.dagron@gmail.com>
2024-07-09 01:15:18 +0200

    update collection

--- src/app/pages/article/article.ts
+++ src/app/pages/article/article.ts
@@ -1,18 +1,15 @@
 export class Article {
-  id: number;
   nom: string;
   categorie: string;
   taille_id: number;
   matiere_id: number;
 
   constructor(
-    id: number,
     nom: string,
     categorie: string,
     taille_id: number,
     matiere_id: number
   ) {
-    this.id = id;
     this.nom = nom;
     this.categorie = categorie;
     this.taille_id = taille_id;
