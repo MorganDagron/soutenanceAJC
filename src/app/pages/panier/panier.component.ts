9d72fe9fd0cd583c1037f8a998dd2e594da290bc
morgan <morgan.dagron@gmail.com>
2024-07-08 11:06:19 +0200

    style page panier

--- src/app/pages/panier/panier.component.ts
+++ src/app/pages/panier/panier.component.ts
@@ -8,35 +8,35 @@ import { Component } from '@angular/core';
 export class PanierComponent {
   panier: any[] = [
     {
-      "image": "https://picsum.photos/200",
+      "image": "https://picsum.photos/100",
       "nom": "Collier en perle blanche",
       "taille": "18 cm",
       "matiere": "Perle blanche",
       "prix": "100 €"
     },
     {
-      "image": "https://picsum.photos/200",
+      "image": "https://picsum.photos/100",
       "nom": "Bague en diamant",
       "taille": "52",
       "matiere": "Diamant",
       "prix": "100 €"
     },
     {
-      "image": "https://picsum.photos/200",
+      "image": "https://picsum.photos/100",
       "nom": "Anneau en rubis",
       "taille": "48",
       "matiere": "Rubis",
       "prix": "100 €"
     },
     {
-      "image": "https://picsum.photos/200",
+      "image": "https://picsum.photos/100",
       "nom": "Collier en émeraude",
       "taille": "20 cm",
       "matiere": "Émeraude",
       "prix": "100 €"
     },
     {
-      "image": "https://picsum.photos/200",
+      "image": "https://picsum.photos/100",
       "nom": "Boucle d'oreille en saphir",
       "taille": "5 mm",
       "matiere": "Saphir",
